c66103084181e914f632f313b3ff7928e182c033
Evgeniy Skvortsov <e.skvortsov@chillgaming.net>
2024-10-29 17:05:01 +0300

    reverse gear logic complete and test sound engine

--- Assets/Code/TankModule/Configs/EngineConfig.cs
+++ Assets/Code/TankModule/Configs/EngineConfig.cs
@@ -7,13 +7,7 @@ namespace Code.TankModule.Configs
     public class EngineConfig : ScriptableObject
     {
         [SerializeField] private Sprite _icon;
-        [SerializeField] private float _maxSpeed;
-        [SerializeField] private float _maxReverseSpeed;
-        [SerializeField] private float _power;
-        
+
         public Sprite Icon => _icon;
-        public float MaxSpeed => _maxSpeed;
-        public float MaxReverseSpeed => _maxReverseSpeed;
-        public float Power => _power;
     }
 }
--- Assets/Code/TankModule/EngineAndTransmission.cs
+++ Assets/Code/TankModule/EngineAndTransmission.cs
@@ -5,23 +5,26 @@ using UnityEngine;
 public class EngineAndTransmission : MonoBehaviour
 {
     public TextMeshProUGUI textInfo;
+    public AudioSource audioSource;
     public Rigidbody rg;
 
     [Header("Engine Settings")]
     public float maxRPM = 7000f; // Максимальные обороты двигателя
+    public float maxReverceRPM = 4000f; // Максимальные обороты двигателя при заднем ходе
     
     public float idleRPM = 1000f; // Обороты холостого хода
     public float engineAcceleration = 300f; // Скорость набора оборотов
     public float engineDeceleration = 200f; // Скорость сброса оборотов
     public float maxEngineTorque = 500f; // Максимальный крутящий момент двигателя
-    public float breakForce = 100f;
+    public float breakForce = 100f; // Тормозная сила
+    public float resistance = 5; //Сопротивление
 
     [Header("Transmission Settings")]
     [Range(0f, 1f)] public float upShiftPercent = 0.9f; //Процент от максимального кол-ва оборотов для повышения передачи
     [Range(0f, 1f)] public float downShiftPercent = 0.5f; //Процент от оборотов холостого хода для понижения передачи
     [Range(0f, 1f)] public float decreaseRmpPercent = 0.7f; //Процент от максимального кол-ва оборотов для повышения передачи
     [Range(0f, 1f)] public float increaseRmpPercent = 0.3f; //Процент от оборотов холостого хода для понижения передачи
-    
+
     public float[] forwardGearRatios; // Передаточные числа каждой передачи
     public float reverseGearRatio = 3.5f; // задняя передача
     public float finalDriveRatio = 3.42f; // Главная передача
@@ -29,9 +32,11 @@ public class EngineAndTransmission : MonoBehaviour
     private float currentRPM = 0f; // Текущие обороты двигателя
     private int currentGear = 0; // Текущая передача
     private float currentEngineTorque = 0f; // Текущий крутящий момент двигателя
-    private float currentWheelForce;
-    private Vector3 previousPosition;
-    private Vector3 currentVelocity;
+    private float currentWheelForce; //Текущая сила передаваемая на колеса
+    private Vector3 previousPosition; //Предыдущая позиция
+    private Vector3 velocity; // Текущий вектор скорости
+
+    private bool IsReverse => currentGear == -1;
 
     private void Start()
     {
@@ -59,7 +64,8 @@ public class EngineAndTransmission : MonoBehaviour
         
         UpdateEngineRPM(gasInput, breakInput);
         HandleGearShift();
-        
+        PlayerEngineSound();
+
     }
 
     private void FixedUpdate()
@@ -70,13 +76,30 @@ public class EngineAndTransmission : MonoBehaviour
 
     private void UpdateEngineRPM(float gasInput, float breakInput)
     {
-        if (gasInput != 0 && breakInput <= 0) //движение вперёд
+        if (gasInput > 0 && breakInput <= 0) //движение вперёд
         {
             currentRPM += Mathf.Abs(gasInput) * engineAcceleration * Time.deltaTime;
+            
+            if (currentGear == -1)
+            {
+                currentGear = 0;
+            }
             
             if (currentGear == 0)
             {
                 currentGear++;
+            }
+        }
+        else if (gasInput < 0) //движение назад
+        {
+            if (velocity.z > 0)
+            {
+                currentRPM -= Mathf.Abs(gasInput) * engineDeceleration * Time.deltaTime;
+            }
+            else
+            {
+                currentRPM += Mathf.Abs(gasInput) * engineAcceleration * Time.deltaTime;
+                currentGear = -1;
             }
         }
         else //торможение
@@ -90,12 +113,22 @@ public class EngineAndTransmission : MonoBehaviour
                 currentRPM -= engineDeceleration * Time.deltaTime;
             }
         }
-        
-        currentRPM = Mathf.Clamp(currentRPM, idleRPM, maxRPM);
+
+        currentRPM = Mathf.Clamp(currentRPM, idleRPM, IsReverse ? maxReverceRPM : maxRPM);
     }
 
     private void HandleGearShift()
     {
+        if (IsReverse)
+        {
+            if (currentRPM <= idleRPM)
+            {
+                currentGear++;
+            }
+            
+            return;
+        }
+        
         if (currentRPM > maxRPM * upShiftPercent && currentGear < forwardGearRatios.Length)
         {
             currentGear++;
@@ -115,22 +148,23 @@ public class EngineAndTransmission : MonoBehaviour
 
     private void CalculateWheelForce()
     {
-        currentEngineTorque = currentRPM / maxRPM * maxEngineTorque;
-
         if (currentGear == 0)
         {
             currentWheelForce = 0;
         }
         else
         {
-            float totalGearRatio =  forwardGearRatios[currentGear - 1] * finalDriveRatio;
-            float wheelTorque = currentEngineTorque / totalGearRatio;
+            var totalGearRatio =  IsReverse
+                ? reverseGearRatio
+                : forwardGearRatios[currentGear - 1] * finalDriveRatio;
+            
+            currentEngineTorque = currentRPM / maxRPM * maxEngineTorque;
+            var wheelTorque = currentEngineTorque / totalGearRatio;
+            currentWheelForce = wheelTorque / resistance;
 
-            // Преобразуем крутящий момент в линейную силу, разделил на корректирующее число (в реальности это радиус колеса)
-            currentWheelForce = wheelTorque / 5;
+            var moveDirection = IsReverse ? Vector3.back : Vector3.forward;
+            rg.MovePosition(transform.position + moveDirection * (currentWheelForce * Time.fixedDeltaTime));
         }
-        
-        rg.MovePosition(transform.position + Vector3.forward * (currentWheelForce * Time.fixedDeltaTime));
 
         ShowDebugInfo();
     }
@@ -139,8 +173,19 @@ public class EngineAndTransmission : MonoBehaviour
     {
         var position = transform.position;
         
-        currentVelocity = (position - previousPosition) / Time.fixedDeltaTime;
+        velocity = (position - previousPosition) / Time.fixedDeltaTime;
         previousPosition = position;
+    }
+    
+    private void PlayerEngineSound()
+    {
+        var minPitch = 1;
+        var maxPitch = 3;
+
+        var normalizedRpm = Mathf.InverseLerp(idleRPM, maxRPM, currentRPM);
+        var currentPitch = Mathf.Lerp(minPitch, maxPitch, normalizedRpm);
+
+        audioSource.pitch = currentPitch;
     }
     
     private void ShowDebugInfo()
@@ -153,6 +198,6 @@ public class EngineAndTransmission : MonoBehaviour
         };
 
         textInfo.text =
-            $"Текущая передача: {gearInfo} \nОбороты: {currentRPM:F0} об/мин \nСила на колеса: {currentWheelForce:F2} Н \nСкорость: {currentVelocity}";
+            $"Текущая передача: {gearInfo} \nОбороты: {currentRPM:F0} об/мин \nСила на колеса: {currentWheelForce:F2} Н \nСкорость: {velocity}";
     }
 }
